73a6c6df7adddd383416821b66d3b2f8f8fe060b
ZhangYY <1443802862@qq.com>
2024-04-07 22:04:35 +0800

    完善启动文件，引入element-plus页面布局

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from '@/router'
+import router from '@/router/index.js'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import zhCn from 'element-plus/es/locale/lang/zh-cn'
 import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import {createPinia} from 'pinia'
+import '@/assets/css/index.css'
 
 const app=createApp(App)
 
